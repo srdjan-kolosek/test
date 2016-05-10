1df1dad817b5c2f30705403d9fc3db220769feac
srdjan-kolosek <srdjan@kolosek.com>
2016-05-10 16:57:11 +0200

    a

---
+++ api/controllers/UserController.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * UserController
+ * @description :: Server-side logic for manage users
+ */
+
+module.exports = {
+  
+};
---
+++ config/services/cipher.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  services: {
+    cipher: {
+      jwt: {
+        secretKey: 'da0c287a6785c94eab20f6097f5a64204a7b59fc4f92190332e00ab7aae1b7ef'
+      }
+    }
+  }
+};
